596c8fe42d02de39eb8a3b2c359f0491eb94f51e
Armand Nobre <nobrearmand@gmail.com>
2022-12-15 09:05:05 +0100

    php

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.typekit.net/ydu5xlm.css">
+</head>
+
+
+<body>
+    <!-- Partie Clément -->
+    <header>
+        <img id="logo" src="img/logo.png" alt="">
+        <div id="header_text">
+            <ul>
+                <a href="#services">Services</a>
+                <a href="#propos">A propos</a>
+            </ul>
+            <div id="contactez">
+                <a style="color:black" href="contact.php">Contactez-Moi</a>
+            </div>
+        </div>
+        <div class="hamburger">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </header>
+    <section id="section_1">
+        <div id="slogan_1">
+            <p>Un accompagnement</p>
+            <p class="text_slogan_2"><span>PERSONNALISÉ</span> parce que</p>
+            <p class="text_slogan_3">Vous êtes <span>UNIQUE</span></p>
+        </div>
+        <div id="slogan_2">
+            <p>Un accompagnement</p>
+            <p><span class="big_text_slogan_1">PERSONNALISÉ</span></p>
+            <p>parce que vous êtes</p>
+            <p><span class="big_text_slogan_2">UNIQUE</span></p>
+        </div>  
+        <div class="intro">
+            <p>Parce vous êtes unique, je vous propose un accompagnement sur mesure, adapté à vos besoins spécifiques pour optimiser votre fiscalité, faire fructifier votre patrimoine, compléter vos revenus, concrétiser vos projets immobiliers, préparer votre retraite, transmettre votre patrimoine …</p>
+            <p>Des solutions existent, je vous aide à les connaitre et déterminer si elles sont adaptées à vos objectifs </p>
+            <p>Vous êtes un entrepreneur, je vous aide à optimiser votre rémunération, optimiser votre trésorerie et vous accompagne dans les étapes de la vie votre entreprise (levée de fonds, développement, transmission, ...</p>
+            <div>
+                <a id="Contactez_moi" href="contact.php"> Contactez moi </a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Partie Armand -->
+    <section class="mes-services" id="services">
+        <h2 id="title-services">Mes Services</h2>
+        <div class="stock-services">
+            <div class="services" id="s1">
+                <img src="img/Bank.png" alt="logo-branque" class="img-services">
+                <h3 class="titre-section-services">Préparer ma <br>retraite</h3>
+                <p class="texte-section-services">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <button class="button-services" id="b1">En Savoir Plus</button>
+            </div>
+            <div class="services" id="s2">
+                <img src="img/Bank.png" alt="logo-branque" class="img-services">
+                <h3 class="titre-section-services">Protection d'un <br>proche</h3>
+                <p class="texte-section-services">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <button class="button-services" id="b2">En Savoir Plus</button>
+            </div>
+            <div class="services" id="s3">
+                <img src="img/Bank.png" alt="logo-branque" class="img-services">
+                <h3 class="titre-section-services">Transmission <br>patrimoniale</h3>
+                <p class="texte-section-services">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <button class="button-services" id="b3">En Savoir Plus</button>
+            </div>
+            <div class="services" id="s4">
+                <img src="img/Bank.png" alt="logo-branque" class="img-services">
+                <h3 class="titre-section-services">Economie <br>d'impôts</h3>
+                <p class="texte-section-services">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <button class="button-services" id="b4">En Savoir Plus</button>
+            </div>
+        </div>
+    </section>
+    <section class="savoir-plus" id="test">
+        <div class="stock-plus">
+            <h2 id="titre-savoir-plus">Préparer Ma Retraite</h2>
+            <p id="texte-savoir-plus">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aperiam adipisci distinctio omnis suscipit esse quasi ipsum doloremque dolor beatae. Magni nulla repellat sit nemo id fugit harum porro aliquam!</p>
+            <a id="btn-savoir-plus" href="contact.php">Discutons de votre Projet</a>
+        </div>  
+    </section>
+    <section class="a-propos" id="propos">
+        <div class="texte-a-propos-stock">
+            <h2 class="titre-a-propos">A Propos</h2>
+            <p class="texte-a-propos">Mon approche est basée sur vos objectifs qui sont uniques et dépendent de qui vous êtes. Il est important de vous écouter et de définir ensemble une stratégie financière et patrimoniale pour réaliser vos projets.</p>
+        </div>
+        <div class="img-a-propos-stock">
+            <img class="img-a-propos" src="img/Rectangle.png" alt="a-propos-img">
+        </div>
+    </section>
+    <main>
+        <div class="mon-experience black-text">
+            <h2 class="pl-4 h2-exp">Mon Expérience</h2>
+            <div class="flex-row experience">
+                <div class="flex-col">
+                    <h3 class="exp-sous-titre h3-exp">Un accompagnement personnalisé, parce que vous êtes UNIQUE</h3>
+                    <p class="bigger-p light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis explicabo officia nam ex minus quasi neque accusantium commodi obcaecati animi, quam dicta. Accusantium, tenetur. Eligendi assumenda deserunt ratione voluptatem vero reiciendis, nobis cupiditate beatae ad ut ab eum provident. Quaerat libero deleniti dolore iste quisquam ullam illum possimus, perferendis ipsa!</p>
+                </div>
+                <div class="flex-col">
+                    <p class="p-exp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nulla est sequi molestiae at! Voluptate laborum voluptas nisi quod porro dolores ducimus, eum ea distinctio autem officia dolore repellendus ut.</p>
+                    <p class="p-exp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit numquam repellat maxime quae libero obcaecati tempora qui nobis nisi ratione saepe perspiciatis provident et tempore temporibus, error impedit sunt nemo molestias voluptas! Molestias, sint! Impedit porro iste ad.</p>
+                    <p class="p-exp">Lorem ipsum dolor sit amet consectetur adipisicing elit. A facilis nemo id corporis? Nobis alias ex quos incidunt officia saepe necessitatibus doloribus quia molestiae ducimus.</p>
+                </div>
+            </div>
+            
+        </div>
+    </main>
+
+    <!-- Partie Ambroise -->
+
+    <footer class="flex-row">
+        <div>
+            <div class="flex-col">
+                <h2 class="h2-exp">Discutons de votre projet !</h2>
+                <p class="p-exp w-400">Vous avez une question d’ordre patrimonial, fiscal ou financier et vous souhaitez obtenir des informations  ? </p>
+                <p class=" w-400 bigger-p">Contactez-moi pour obtenir la meilleure solution !</p>
+            </div>
+            <a class="pad-top contact light-button" href="contact.php">Contactez-Moi !</a>
+        </div>
+        <div class="flex-col">
+            <div>
+                <div class="flex-row footer_contact">
+                    <img class="logo-footer" src="img/logo_2.png" alt="">
+                    <div class="flex-col">
+                        <div class="flex-row">
+                            <img src="img/phone.svg" alt="">
+                            <p>06.64.30.35.89</p>
+                        </div>
+                        <div class="flex-row">
+                            <img src="img/envelope.svg" alt="">
+                            <p>tm38.conseil.finance@gmail.com</p>
+                        </div>
+                        <div class="flex-row flex-icon">
+                            <iconify-icon icon="bi:linkedin" width="32" height="32"></iconify-icon>
+                            <iconify-icon icon="bi:instagram" width="32"></iconify-icon>
+                            <iconify-icon icon="bi:facebook"  width="32" height="32"></iconify-icon>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+            <div class="flex-row confidentialite">
+                <p>Politique de confidentialité</p>
+                <p>Mentions légales</p>
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
+    <script src="js.js"></script>
+    <script src="burger.js"></script>
+</body>
+</html>
